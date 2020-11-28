bcd92124330287d3ca1f47402d254f4f137d10e7
Flobul <flobul.jeedom@gmail.com>
2020-11-28 19:22:31 +0100

    Add files via upload

--- desktop/php/espeasy.php
+++ desktop/php/espeasy.php
@@ -4,6 +4,7 @@ if (!isConnect('admin')) {
   throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'espeasy');
+$plugin = plugin::byId('espeasy');
 $eqLogics = eqLogic::byType('espeasy');
 $state = config::byKey('include_mode', 'espeasy');
 echo '<div id="div_inclusionAlert"></div>';
@@ -64,12 +65,14 @@ if ($state == 1) {
       <?php
       foreach ($eqLogics as $eqLogic) {
         $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-        echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-        echo "<center>";
-        echo '<img src="plugins/espeasy/plugin_info/espeasy_icon.png" height="105" width="95" />';
-        echo "</center>";
-        echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-        echo '</div>';
+        echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 180px !important;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 180px;margin-left : 10px;' . $opacity . '" >';
+				if ($eqLogic->getConfiguration('icone') != '') {
+					echo '<img src="plugins/espeasy/plugin_info/node_'.$eqLogic->getConfiguration('icone').'.png">';
+				} else echo '<img src="plugins/espeasy/plugin_info/espeasy_icon.png" height="105" width="95" />';
+				echo '<br>';
+				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '<span style="font-size:10px" class="subname">(' . $eqLogic->getConfiguration('ip') . ')</span>';
+				echo '</div>';
       }
       ?>
     </div>
