484331c6b1d850fb9aff605bd28acea4b9b7a6a4
Flobul <flobul.jeedom@gmail.com>
2021-01-01 20:01:48 +0100

    Add files via upload

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -47,10 +47,14 @@ foreach ($eqLogics as $eqLogic) {
 	}
 	echo '<td>' . $status . '</td>';
 	$battery_status = '<span class="label label-success" style="font-size : 1em;">{{OK}}</span>';
-	$battery = $eqLogic->getConfiguration('batteryStatus');
+	if (($eqLogic->getConfiguration('batteryStatus') == '') && is_object($eqLogic->getCmd('info', 'Battery'))) {
+		$battery = $eqLogic->getCmd('info', 'Battery')->execCmd();
+    } else {
+		$battery = $eqLogic->getConfiguration('batteryStatus');
+	}
 	if ($battery == '') {
 		$battery_status = '<span class="label label-primary" style="font-size : 1em;" title="{{Secteur}}"><i class="fas fa-plug"></i></span>';
-  } elseif ($battery < 20) {
+	} elseif ($battery < 20) {
 		$battery_status = '<span class="label label-danger" style="font-size : 1em;">' . $battery . '%</span>';
 	} elseif ($battery < 60) {
 		$battery_status = '<span class="label label-warning" style="font-size : 1em;">' . $battery . '%</span>';
@@ -60,7 +64,12 @@ foreach ($eqLogics as $eqLogic) {
 		$battery_status = '<span class="label label-primary" style="font-size : 1em;">' . $battery . '%</span>';
 	}
 	echo '<td>' . $battery_status . '</td>';
-	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
+	$idleMax = new DateTime("1 week ago");
+	$tsIdleMax = $idleMax->getTimestamp();
+	$lastCommunication = strtotime($eqLogic->getStatus("lastCommunication"));
+	$dateFR = date("d/m/Y \à H\hi", $lastCommunication);
+	$messageLevel = $lastCommunication < $tsIdleMax ? $label = "warning" : $label = "info";
+	echo '<td><span class="label label-'.$label.'" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
 }
 ?>
